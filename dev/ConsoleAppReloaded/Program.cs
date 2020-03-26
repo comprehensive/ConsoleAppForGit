d4f7e53fdd2a4c192e05879dcf121d63fccf8955
comprehensive <anton.spass@gmail.com>
2020-03-26 12:14:18 +0300

    [anton] basic structure added

---
+++ dev/ConsoleAppReloaded/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using ConsoleAppReloaded.Domain;
+
+namespace ConsoleAppReloaded
+{
+    class Program
+    {
+        static void Main()
+        {
+            Feeder feeder = new Feeder();
+            Console.WriteLine(feeder.Talk());
+            Console.ReadLine();
+        }
+    }
+}
---
+++ dev/Domain/Feeder.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppReloaded.Domain
+{
+    public class Feeder
+    {
+        public string Talk()
+        {
+            string result = string.Format("Hello world {0}", DateTime.Now);
+            return result;
+        }
+    
+    }
+}
